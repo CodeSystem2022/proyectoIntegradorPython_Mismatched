79a0a39b67d5543105ca3f5b703d809b0739b7ea
Santiago Martos <SantiMartos30@gmail.com>
2023-06-29 20:24:41 -0300

    add: calculadoraEdad model and calculadoraEdad service

---
+++ capa_datos_/calculadoraEdad.py
@@ -0,0 +1,90 @@
+from datetime import date
+
+
+class CalculadoraEdad:   # Creamos la clase
+    def __init__(self):
+        self.combinacionNA = None
+        self.nombre = ""
+        self.apellido = ""
+        self.dia = 0
+        self.mes = 0
+        self.anio = 0
+
+    # Menu
+    def menuCalculadoraEdad(self):
+        print("Calculadora de Edad")
+        print("--------------------")
+        self.nombre = input("Ingrese su nombre: ")
+        self.apellido = input("Ingrese su apellido: ")
+        self.combinacionNA = self.nombre + (" ") + self.apellido
+        print(self.combinacionNA)
+        print("--------------------")
+        self.dia = int(input("Ingresa tu día de nacimiento: "))
+        self.mes = int(input("Ingresa tu mes de nacimiento: "))
+        self.anio = int(input("Ingresa tu año de nacimiento: "))
+
+    # Metodo para calcular la edad
+    def calcular_edad(self):
+        hoy = date.today()   # Se usa datetime para identificar la fecha actual
+        fecha_nacimiento = date(self.anio, self.mes, self.dia)
+        edad = hoy.year - fecha_nacimiento.year
+
+        # Verificamos si el cumpleaños ya ha pasado este año
+        if (hoy.month, hoy.day) < (fecha_nacimiento.month, fecha_nacimiento.day):
+            edad -= 1   # Si el dia y mes de hoy, es menor al mes y dia de nacimiento, se le resta uno (Para poder expresarlo en meses y dias)
+
+        # Calculamos el cumpleaños
+        cumpleanios = fecha_nacimiento.replace(year=hoy.year)
+        dias_restantes = (cumpleanios - hoy).days
+
+        # Si la fecha actual es mayor a la fecha de nacimiento:
+        if hoy.month > fecha_nacimiento.month:
+            meses = hoy.month - fecha_nacimiento.month
+            dias = hoy.day - fecha_nacimiento.day
+
+        # Si el mes actual es menor al mes de nacimiento:
+        elif hoy.month < fecha_nacimiento.month:
+            meses = hoy.month + 12 - fecha_nacimiento.month   # Se calculan los meses
+            dias = hoy.day - fecha_nacimiento.day   # Se calcula los dias
+
+        # Si el dia actual es mayor o igual al dia de la fecha de nacimiento:
+        elif hoy.day >= fecha_nacimiento.day:
+            meses = 0
+            dias = hoy.day - fecha_nacimiento.day   # Se calculan los dias
+
+        # Si no se cumple ninguna de las condiciones:
+        else:
+            meses = 11
+            dias = hoy.day + 30 - fecha_nacimiento.day
+
+        # Si los dias son negativos, se le resta 1 mes (para agregarle dias en vez de meses), se convierte a positivo y se ajusta la fecha
+        if dias < 0:
+            meses -= 1
+            dias *= -1
+            dias += 30 - dias
+
+        # Se ajustan los dias restantes en caso de que el cumpleaños ya haya pasado
+        if dias_restantes < 0:
+            cumpleanios_proximo_anio = fecha_nacimiento.replace(year=hoy.year + 1)   # Se reemplaza el año y se agrega 1
+            dias_restantes = (cumpleanios_proximo_anio - hoy).days
+
+        return edad, meses, dias, dias_restantes
+
+    # Para mostrar el resultado del cálculo
+    def mostrar_resultado(self, edad, meses, dias, dias_restantes):
+        if dias_restantes == 0:   #
+            print(f"\n¡Feliz cumpleaños, {self.nombre}!")
+            print(f"¡Hoy cumpliste {edad} años!")
+        else:
+            print(f"\nTu edad es: {edad} años, {meses} meses y {dias} días.")
+            print(f"Faltan {dias_restantes} días para tu próximo cumpleaños.")
+
+    def ingresar_datos(self):
+        self.menuCalculadoraEdad()
+        edad, meses, dias, dias_restantes = self.calcular_edad()
+        self.mostrar_resultado(edad, meses, dias, dias_restantes)
+
+
+if __name__ == '__main__':
+    calculadora = CalculadoraEdad()
+    calculadora.ingresar_datos()
